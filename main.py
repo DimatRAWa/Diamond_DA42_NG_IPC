f32560fd5533b0a4fb3551eac4b4cd29869c25ae
DimatRAWa <crossdark@mail.ru>
2024-06-13 13:07:44 +0300

    One more changes into settings

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import settings
 
 bot = telebot.TeleBot(settings.TOKEN)
 
-@bot.message_handler(commands=["start"])
+@bot.message_handler(commands=["хай"])
 def start(m, res = False):
     bot.send_message(m.chat.id, 'Отправьте P/N')
 
