6f4114642ad9f1452d231e9465922f1777617df6
DimatRAWa <crossdark@mail.ru>
2024-07-01 12:48:46 +0300

    added a working code

--- main.py
+++ main.py
@@ -4,31 +4,22 @@ import settings
 
 bot = telebot.TeleBot(settings.TOKEN)
 
-excel_file = pd.read_excel(settings.file)
-df = pd.DataFrame(excel_file, columns = ['P/N', 'ATA&Pos'])
-print(df)
+def load_orders():
+    excel_file = settings.file
+    sheet_name = 'Лист1'
+    df = pd.read_excel(excel_file, sheet_name = sheet_name)
+    df['P/N'] = df['P/N'].astype(str)
+    return df
 
 @bot.message_handler(commands = ['start'])
-def start(m, res = False):
-    bot.send_message(m.chat.id, 'Отправьте P/N')
+def handle_start(message):
+    bot.reply_to(message, 'Привет! Отправь P/N изделия.')
 
-@bot.message_handler(content_types=['text'])
-def get_text_message(message):
-    if message.text == 'A508-28' or message.text == 'a508-28':
-        bot.send_message(message.chat.id, '33-40-03-1_Pos.120')
-    elif message.text == '02-0250276-00':
-        bot.send_message(message.chat.id, '33-40-03-1_Pos.100')
-    elif message.text == '01-0770062-05':
-        bot.send_message(message.chat.id, '33-40-03-1_Pos.200')
-    elif message.text == 'W1290-28':
-        bot.send_message(message.chat.id, '33-40-03-1_Pos.110')
-    elif message.text == 'BW1-B12-RY8-RY6-150':
-        bot.send_message(message.chat.id, '32-10-02-1_Pos.690')
-    elif message.text == 'BW1-B14-RB6-RB6-200':
-        bot.send_message(message.chat.id, '32-20-01-1_Pos.650')
-    elif message.text == '/help':
-        bot.send_message(message.chat.id, 'Напиши P/N')
-    else:
-        bot.send_message(message.chat.id, 'Я тебя не понимаю, напиши /help.')
+@bot.message_handler(func=lambda message: True)
+def handle_text(message):
+    order_number = message.text.strip()
+    orders_df = load_orders()
+    position = orders_df.loc[orders_df['P/N'] == order_number, 'Position'].value
+    bot.reply_to(message, f'Позиция {order_number}: {position}')
 
 bot.polling(none_stop = True, interval = 0)
